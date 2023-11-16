c8ff70b77f6eed60bfe823925dc0a6ee3767da7f
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-16 21:28:10 +0100

    RAS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-  `
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    template: `
+        <h1>Welcome to {{pokemonListe[1]}}!</h1>
+    `
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+    title = 'Pokemon';
+    pokemonListe = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+    ngOnInit() {
+        console.table(this.pokemonListe);
+    }
 }
