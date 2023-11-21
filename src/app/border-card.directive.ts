cef8074f3f74a30b43bdca948e65dcc8556001d2
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-21 21:36:28 +0100

    Directive Fait

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -4,24 +4,30 @@ import {Directive, ElementRef, HostListener, Input} from '@angular/core';
   selector: '[pkmnBorderCard]',
   standalone: true
 })
+
+
 export class BorderCardDirective {
 
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
+
   constructor(private el: ElementRef) {
-    this.setHeight(180);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
 
   //déclaration borderColor pour
   @Input('pkmnBorderCard') borderColor: string;
-
   //changement couleur bordure quand curseur sur la carte
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   //renitialisation lorsque curseur sort de la carte.
   @HostListener('mouseleave') onMouseLeave(){
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   //définir hauteur des cartes
