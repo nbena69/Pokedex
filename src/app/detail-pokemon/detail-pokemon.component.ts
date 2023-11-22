89975594ae569cf68b1d573e29d34d0056bc5e64
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-22 17:45:21 +0100

    Creation component detail et list

---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detail-pokemon.component.html',
+  styles: ``
+})
+export class DetailPokemonComponent {
+
+}
---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-pokemon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-pokemon.component.html',
+  styles: ``
+})
+export class ListPokemonComponent {
+
+}
