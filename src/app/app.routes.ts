64a35afdbb9faaa953e50c2a60b5de4d2b23954e
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-22 18:57:07 +0100

    Methode redirection vers carte et goBack fonctionnelle.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NgModule } from "@angular/core";
 import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
 import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
     { path: 'pokemons', component: ListPokemonComponent },
     { path: 'pokemon/:id', component: DetailPokemonComponent},
-    { path: '', redirectTo: 'pokemons', pathMatch: 'full'}
+    { path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent}
 ];
