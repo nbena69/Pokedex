5375b3e6511c1995e3aef4a61c79e689f93de72c
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-22 19:12:28 +0100

    MAJ ALL IMPORT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { NgModule } from "@angular/core";
-import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
-import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {ListPokemonComponent} from "./pokemon/list-pokemon/list-pokemon.component";
+import {DetailPokemonComponent} from "./pokemon/detail-pokemon/detail-pokemon.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 export const routes: Routes = [
