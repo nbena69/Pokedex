35fa2255aeb9a9e00734eab2bc44f491ed8ada5b
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-22 17:45:05 +0100

    Pipes fait (type des pokemons)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from "./app.component";
 import { BorderCardDirective} from "./border-card.directive";
 import { PokemonTypeColorPipe} from "./pokemon-type-color.pipe";
+import { ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
 
 
 @NgModule({
   declarations: [
-      AppComponent,
-      BorderCardDirective,
-      PokemonTypeColorPipe
   ],
   imports: [
       BrowserModule,
