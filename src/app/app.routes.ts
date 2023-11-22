ea90ffa113821fa250caeea3ed99ca81c939cd91
Na'el Benaïssa <126596584+nbena69@users.noreply.github.com>
2023-11-22 18:12:14 +0100

    Creation component detail et list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
+import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pokemons', component: ListPokemonComponent },
+    { path: 'pokemon/:id', component: DetailPokemonComponent},
+    { path: '', redirectTo: 'pokemons', pathMatch: 'full'}
+];
