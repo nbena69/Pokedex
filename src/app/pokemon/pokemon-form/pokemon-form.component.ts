b9a78378e6da54de408f9abf750c1af47d11a68e
nbena69 <naelbenaissapro@gmail.com>
2023-11-26 16:36:04 +0100

    Ajout CSS Form

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -10,7 +10,8 @@ import {PokemonTypeColorPipe} from "../pokemon-type-color.pipe";
     selector: 'app-pokemon-form',
     standalone: true,
     imports: [CommonModule, FormsModule, PokemonTypeColorPipe],
-    templateUrl: './pokemon-form.component.html'
+    templateUrl: './pokemon-form.component.html',
+    styleUrls: ['./pokemon-form.component.css']
 })
 export class PokemonFormComponent implements OnInit {
     @Input() pokemon: Pokemon;
@@ -38,9 +39,23 @@ export class PokemonFormComponent implements OnInit {
         }
     }
 
+    isTypesValid(type: string): boolean{
+        if(this.pokemon.types.length == 1 && this.hasType(type)) {
+            return false;
+        }
+
+        if(this.pokemon.types.length > 2 && !this.hasType(type)) {
+            return false;
+        }
+
+        return true;
+    }
+
     onSubmit() {
         console.log('Fourmulaire ajouter !')
         this.router.navigate(['/pokemon', this.pokemon.id]);
     }
+
+
 
 }
