89b529155508527dc22670a016b000a5dd72f64e
nbena69 <naelbenaissapro@gmail.com>
2023-11-26 15:21:03 +0100

    Service avec méthode fini

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Pokemon} from "./pokemon";
+import {POKEMONS} from "./mock-pokemon-list";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+    getPokemmonList(): Pokemon[] {
+        return POKEMONS;
+    }
+
+    getPokemonById(pokemonId: number): Pokemon | undefined {
+        return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+    }
+
+    getPokemonTypeList(): string[] {
+        return [
+            'Plante',
+            'Feu',
+            'Eau',
+            'Insecte',
+            'Normal',
+            'Electrik',
+            'Poison',
+            'Fée',
+            'Vol',
+            'Combat',
+            'Psy'
+        ]
+    }
 }
