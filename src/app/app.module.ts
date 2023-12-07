267856098d43ca602d40a2d47a0fd59459532cad
nbena69 <naelbenaissapro@gmail.com>
2023-12-07 19:20:29 +0100

    RAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {PokemonModule} from './pokemon/pokemon.module';
 import {AppRoutes} from "./app.routes";
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     declarations: [
@@ -16,6 +17,7 @@ import {FormsModule} from "@angular/forms";
     imports: [
         BrowserModule,
         FormsModule,
+        HttpClientModule,
         PokemonModule,
         AppRoutes
     ],
